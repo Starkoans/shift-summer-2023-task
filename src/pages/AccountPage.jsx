a6454fb61b304a87e563da11143259a1db0547e2
starkoans <tasya16mosya@gmail.com>
2023-07-12 19:26:02 +0600

    FIx: auth check for contetnt

--- src/pages/AccountPage.jsx
+++ src/pages/AccountPage.jsx
@@ -1,11 +1,20 @@
+import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 
 import AccountHeader from '../components/Account/AccountHeader/AccountHeader.jsx';
 
 function AccountPage() {
+  const user = useSelector(state => state.user);
+
   return (
-    <div>
-      <AccountHeader />
+    <div className="bg-gray-100">
+      {user.id ? (
+        <AccountHeader />
+      ) : (
+        <p className="text-center p-10">
+          Авторизируйтесь, чтобы просмотреть профиль.
+        </p>
+      )}
     </div>
   );
 }
