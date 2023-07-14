654473ee1e8e0202590f0c133869c38769cc07ae
starkoans <tasya16mosya@gmail.com>
2023-07-14 20:21:39 +0600

    add: select delivery type

--- src/components/OrderOptionsForm/SelectDeliveryType.jsx
+++ src/components/OrderOptionsForm/SelectDeliveryType.jsx
@@ -1,29 +1,45 @@
 import { useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { setDeliveryOption } from '../../store/newDelivery.slice.js';
 
 function SelectDeliveryType() {
+  const dispatch = useDispatch();
   const newDelivery = useSelector(state => state.newDelivery);
   const [selectedType, setSelectedType] = useState('DEFAULT');
+  const handleSelect = option => {
+    setSelectedType(option.type);
+    dispatch(
+      setDeliveryOption({
+        id: option.id,
+        price: option.price,
+        days: option.days,
+        name: option.name,
+        type: option.type,
+      })
+    );
+  };
+
   return (
     <section className="flex flex-row">
-      {newDelivery.calc.options.map((o, key) => {
+      {newDelivery.calc.options.map((option, index) => {
         return (
           <div
-            onClick={e => setSelectedType(o.type)}
+            onClick={e => handleSelect(option)}
             className={
-              selectedType === o.type
+              selectedType === option.type
                 ? 'bg-purple-800 text-white m-3 p-5 border-2 rounded-2xl w-1/2 '
                 : 'm-3 p-5 border-2 rounded-2xl w-1/2 hover:bg-gray-200'
             }
-            key={key}
+            key={index}
           >
-            {o.type === 'DEFAULT' ? (
+            {option.type === 'DEFAULT' ? (
               <p> Обычная доставка </p>
             ) : (
               <p>Экспресс доставка</p>
             )}
-            <p className="font-black text-2xl">{o.price} Р</p>
-            <p>{o.days} рабочих дней</p>
+            <p className="font-black text-2xl">{option.price} Р</p>
+            <p>{option.days} рабочих дней</p>
           </div>
         );
       })}
