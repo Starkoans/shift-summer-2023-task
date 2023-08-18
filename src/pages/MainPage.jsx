2e0644cfdd7bb5e2d5923fbb080b71e7aaac2211
starkoans <tasya16mosya@gmail.com>
2023-08-18 18:00:58 +0600

    feat: адаптивность + дизайн

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,10 +1,60 @@
+import { useEffect, useState } from 'react';
+
+import CartboardBoxes from '../../public/img/Cartboard_boxes_with_list.png';
+import Slogan from '../../public/img/Slogan.png';
 import CalculateDelivery from '../components/CalculateDelivery/CalculateDelivery.jsx';
 
-function MainPage() {
+export default function MainPage() {
+  const [isMatches, setIsMatches] = useState(
+    window.matchMedia('(min-width: 850px)').matches
+  );
+  useEffect(() => {
+    window
+      .matchMedia('(min-width: 850px)')
+      .addEventListener('change', e => setIsMatches(e.matches));
+  }, []);
+
   return (
-    <div className="flex justify-center">
-      <CalculateDelivery />
+    <div
+      className={
+        isMatches
+          ? 'flex justify-center self-center h-screen mx-20'
+          : 'flex flex-col justify-center self-center mt-10'
+      }
+    >
+      {
+        <div
+          className={
+            isMatches
+              ? 'relative m-10 self-center align-middle h-fit'
+              : 'relative self-center align-middle h-fit'
+          }
+        >
+          <div className={isMatches ? 'relative scale-125' : 'relative'}>
+            <img
+              src={Slogan}
+              alt="SHIFT DELIVERY - быстро, удобно, надежно!"
+              className=" absolute inset-0 m-auto animate-spin-slow"
+            />
+            <div
+              className="scale-75 relative inset-0 w-80 h-80 m-auto rounded-full bg-purple-950
+            bg-gradient-to-br from-fuchsia-300 to-purple-500"
+            />
+          </div>
+          <img
+            src={CartboardBoxes}
+            alt="Delivery illustation"
+            className={
+              isMatches
+                ? 'absolute m-auto inset-0 ease-linear duration-500 hover:transition-transform hover:ease-linear hover:scale-125'
+                : 'absolute m-auto inset-0 ease-linear duration-500 scale-75 hover:transition-transform hover:ease-linear hover:scale-90'
+            }
+          />
+        </div>
+      }
+      <div className={isMatches ? 'self-center ml-10' : 'self-center my-20'}>
+        <CalculateDelivery />
+      </div>
     </div>
   );
 }
-export default MainPage;
